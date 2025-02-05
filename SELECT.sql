7425fe542ad9a7c5dd2abc0b34e7c95e3c3fe782
Neto test <akochetkov1312@gmail.com>
2025-02-05 16:05:44 +0300

    Commit

--- SELECT.sql
+++ SELECT.sql
@@ -27,29 +27,29 @@ WHERE name ILIKE 'my %'
 select musical_genre_id , count(executor_id) from executor_musical_genre
 group by musical_genre_id;
 
-select  count (t.name, t.year) from track_list t
-join albums_title a on s.id_of_album_title  = a.id
-where year between '2017-12-31' and '2021-01-01';
+select  count (t.name) as count_name
+from track_list t
+join albums_title a on a.album_title_id  = a.id
+where year between '2018-12-31' and '2021-01-01';
 
 
-select at.name, avg(duration) from albums_title at
-left join track_list tl on at.id = tl.id_of_album_title 
+select at.name, avg(duration) from album_title at
+left join track_list tl on at.id = tl.album_title_id 
 group by at.name;
 
 
-select nickname from executor e
-where nickname not in (
-   select nickname from executor e
-   join executor e on eat.executor_album_title_id=eat.id 
-   join executor_album_title eat on eat.album_title_id=eat.id 
-   where extract(YEAR from year) = 2020);   
+SELECT e.id, e.nickname
+FROM executor e
+LEFT JOIN album_title a ON e.id = a.artist_id AND a.year = 2020
+WHERE a.id IS NULL;  
 
 
-select distinct c.name, e2.nickname from collection c
-left join collection_track_list ctl on ctl.collection_id = c.id 
-left join track_list tl on ctl.track_list_id = tl.id 
-left join album_title at on e2.id_of_album_title = at.id 
-left join executor_album_title eat on at.id = eat.album_title_id 
-left join executor e2 on eat.executor_id = e2.id 
-where e2.nickname = 'Eminem';
+SELECT DISTINCT c.name, e2.nickname 
+FROM collection c
+LEFT JOIN collection_track_list ctl ON ctl.collection_id = c.id 
+LEFT JOIN track_list tl ON ctl.track_list_id = tl.id 
+LEFT JOIN executor_album_title eat ON tl.id = eat.track_list_id 
+LEFT JOIN album_title at ON eat.album_title_id = at.id 
+LEFT JOIN executor e2 ON eat.executor_id = e2.id 
+WHERE e2.nickname = 'Eminem';
 
